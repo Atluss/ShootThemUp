ccc6b0afa6288f3e76eb8393ed5720d2bce94e43
Shokin Ilya <atlus3@mail.ru>
2021-06-22 08:36:52 +0300

    add: fist commit

---
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -0,0 +1,49 @@
+// Shoot Them Up Game. All rights reserved.
+
+
+#include "Components/STUHealthComponent.h"
+#include "Dev/STUFireDamageType.h"
+#include "Dev/STUIceDamageType.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogHealthController, All, All);
+
+// Sets default values for this component's properties
+USTUHealthComponent::USTUHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+// Called when the game starts
+void USTUHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Health = MaxHealth;
+
+    AActor* ComponentOwner = GetOwner();
+
+    if (ComponentOwner)
+    {
+        ComponentOwner->OnTakeAnyDamage.AddDynamic(this, &USTUHealthComponent::OnTakeAnyDamage);
+    }
+	
+}
+
+void USTUHealthComponent::OnTakeAnyDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy,
+    AActor* DamageCauser)
+{
+    Health -= Damage;
+    UE_LOG(LogHealthController, Warning, TEXT("Damage power: %f"), Damage);
+
+    if (DamageType)
+    {
+        if (DamageType->IsA<USTUFireDamageType>())
+        {
+            UE_LOG(LogHealthController, Warning, TEXT("So hooooooooot !!!"));
+        } else if (DamageType->IsA<USTUIceDamageType>())
+        {
+            UE_LOG(LogHealthController, Warning, TEXT("So coooooooold !!!"));
+        }
+    }
+}
---
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -0,0 +1,12 @@
+// Shoot Them Up Game. All rights reserved.
+
+
+#include "STUGameModeBase.h"
+#include "Player/STUBaseCharacter.h"
+#include "Player/STUPlayerController.h"
+
+ASTUGameModeBase::ASTUGameModeBase()
+{
+    DefaultPawnClass = ASTUBaseCharacter::StaticClass();
+    PlayerControllerClass = ASTUPlayerController::StaticClass();
+}
