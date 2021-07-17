e44dcd10eb8ffb0680ee52c5caaebf02eb76f6d5
Shokin Ilya <atlus3@mail.ru>
2021-07-17 17:17:04 +0300

    add: pickup respawn

--- Source/ShootThemUp/Public/Pickups/STUBasePickup.h
+++ Source/ShootThemUp/Public/Pickups/STUBasePickup.h
@@ -22,6 +22,9 @@ protected:
     UPROPERTY(VisibleAnywhere, Category="Pickup")
     USphereComponent* CollisionComponent;
     
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
+    float RespawnTime = 5.0f;
+    
 	virtual void BeginPlay() override;
     virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
 
@@ -29,4 +32,8 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+    virtual bool GivePickupTo(APawn* PlayerPawn);
+    void PickupWasTaken();
+    void Respawn();
 };
