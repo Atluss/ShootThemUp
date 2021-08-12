e2f01a4e6def374f8fd6f3f6f86b2223422a2275
Shokin Ilya <atlus3@mail.ru>
2021-08-12 07:57:51 +0300

    add: HUD for time remaining and stats

---
+++ Source/ShootThemUp/Private/UI/STUGameDataWidget.cpp
@@ -0,0 +1,41 @@
+// Shoot Them Up Game. All rights reserved.
+
+
+#include "UI/STUGameDataWidget.h"
+#include "STUGameModeBase.h"
+#include "Player/STUPlayerState.h"
+
+int USTUGameDataWidget::GetKillsNum() const
+{
+    const auto PlayerState = GetStuPlayerState();
+
+    return PlayerState ? PlayerState->GetKillsNum() : 0;
+}
+
+int32 USTUGameDataWidget::GetCurrentRoundNum() const
+{
+    const auto GameMode = GetSTUGameModeBase();
+    return GameMode ? GameMode->GetCurrentRoundNum() : 0;
+}
+
+int32 USTUGameDataWidget::GetTotalRoundNum() const
+{
+    const auto GameMode = GetSTUGameModeBase();
+    return GameMode ? GameMode->GetGameData().RoundsNum : 0;
+}
+
+int32 USTUGameDataWidget::GetRoundSecondRemaining() const
+{
+    const auto GameMode = GetSTUGameModeBase();
+    return GameMode ? GameMode->GetRoundSecondsRemaining() : 0;
+}
+
+ASTUGameModeBase* USTUGameDataWidget::GetSTUGameModeBase() const
+{
+    return GetWorld() ? Cast<ASTUGameModeBase>(GetWorld()->GetAuthGameMode()) : nullptr;
+}
+
+ASTUPlayerState* USTUGameDataWidget::GetStuPlayerState() const
+{
+    return GetOwningPlayer() ? Cast<ASTUPlayerState>(GetOwningPlayer()->PlayerState) : nullptr;
+}
