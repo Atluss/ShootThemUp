7a36dada0c0af0d0bf7bb1bc73d5daff8a40866a
Shokin Ilya <atlus3@mail.ru>
2021-08-27 07:03:39 +0300

    add: level loading screen

--- Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
+++ Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
@@ -28,12 +28,19 @@ void USTUMenuWidget::NativeOnInitialized()
     InitLevelItems();
 }
 
-void USTUMenuWidget::OnStartGame()
+void USTUMenuWidget::OnAnimationFinished_Implementation(const UWidgetAnimation* MovieSceneBlends)
 {
+    // Super::OnAnimationFinished_Implementation(MovieSceneBlends);
+    if (MovieSceneBlends != HideAnimation) return;
     const auto STUGameInstance = GetSTUGameInstance();
     if (!STUGameInstance) return;
     
     UGameplayStatics::OpenLevel(this, STUGameInstance->GetStartupLevel().LevelName);
+}
+
+void USTUMenuWidget::OnStartGame()
+{
+    PlayAnimation(HideAnimation);
 }
 
 void USTUMenuWidget::OnQuitGame()
