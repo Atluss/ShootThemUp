f81cec01b23f451329ac9f37f24c2c24edfd90b3
Shokin Ilya <atlus3@mail.ru>
2021-08-29 08:53:53 +0300

    add: menu sound and locomotion sounds

--- Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
+++ Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
@@ -8,6 +8,7 @@
 #include "STUGameInstance.h"
 #include "Components/HorizontalBox.h"
 #include "Menu/UI/STULevelItemWidget.h"
+#include "Sound/SoundCue.h"
 
 
 DEFINE_LOG_CATEGORY_STATIC(LogSTUMenuWidget, All, All);
@@ -41,6 +42,7 @@ void USTUMenuWidget::OnAnimationFinished_Implementation(const UWidgetAnimation* 
 void USTUMenuWidget::OnStartGame()
 {
     PlayAnimation(HideAnimation);
+    UGameplayStatics::PlaySound2D(GetWorld(), StartGameSound);
 }
 
 void USTUMenuWidget::OnQuitGame()
